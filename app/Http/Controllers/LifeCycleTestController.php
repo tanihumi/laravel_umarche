555b3a7f62e564eb600693f1e64eac5ca5ba902d
tanihumi <x7azzsy@gmail.com>
2023-04-15 15:48:10 +0900

    サービスコンテナ

---
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    //
+    public function showServiceContainerTest()
+    {
+        app()->bind('lifeCycleTest', function () {
+            return 'ライフサイクルテスト';
+        }) ;
+
+        $test = app()->make('lifeCycleTest');
+
+        $message = new Message();
+        $sample = new Sample($message);
+        $sample->run();
+
+        dd($test, app());
+    }
+}
+
+class sample
+{
+    public $message;
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+    public function run()
+    {
+        $this->message->send();
+    }
+}
+
+class Message
+{
+    public function send()
+    {
+        echo 'メッセージ表示';
+    }
+}
